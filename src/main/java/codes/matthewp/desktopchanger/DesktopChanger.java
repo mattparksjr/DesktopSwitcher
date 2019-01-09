d6f896c122755e8f4e236af45d2e8d156cc37e34
Matthew Parks <mattparksjr@gmail.com>
2019-01-08 15:11:50 -0900

    Created the base for changing the desktop background. - Uses JNA to access windows User32 library - JNA maps our function call to windows - Switches background :)

--- src/main/java/codes/matthewp/desktopchanger/DesktopChanger.java
+++ src/main/java/codes/matthewp/desktopchanger/DesktopChanger.java
@@ -1,6 +1,8 @@
 package codes.matthewp.desktopchanger;
 
 import codes.matthewp.desktopchanger.configuration.Configuration;
+import codes.matthewp.desktopchanger.core.SPI;
+import com.sun.jna.platform.win32.WinDef;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
@@ -11,8 +13,9 @@ import javafx.stage.Stage;
 public class DesktopChanger extends Application {
 
     private static Configuration config;
+
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
@@ -20,9 +23,17 @@ public class DesktopChanger extends Application {
         Platform.setImplicitExit(false);
     }
 
-
     public static void main(String[] args) {
         config = new Configuration();
+        setBackground("C:\\Users\\Matthew\\Downloads\\imageproxy (2).jpg");
         launch(args);
+    }
+
+    public static void setBackground(String path) {
+        SPI.INSTANCE.SystemParametersInfo(
+                new WinDef.UINT_PTR(SPI.SPI_SETDESKWALLPAPER),
+                new WinDef.UINT_PTR(0),
+                path,
+                new WinDef.UINT_PTR(SPI.SPIF_UPDATEINIFILE | SPI.SPIF_SENDWININICHANGE));
     }
 }
---
+++ src/main/java/codes/matthewp/desktopchanger/core/SPI.java
@@ -0,0 +1,32 @@
+package codes.matthewp.desktopchanger.core;
+
+import com.sun.jna.Native;
+import com.sun.jna.platform.win32.WinDef;
+import com.sun.jna.win32.*;
+
+import java.util.HashMap;
+
+
+public interface SPI extends StdCallLibrary {
+
+    long SPI_SETDESKWALLPAPER = 20;
+    long SPIF_UPDATEINIFILE = 0x01;
+    long SPIF_SENDWININICHANGE = 0x02;
+
+    /**
+     * Loads the Windows User32 Library, maps our call to the windows api then calls it
+     */
+    SPI INSTANCE = Native.loadLibrary("user32", SPI.class, new HashMap<String, Object>() {
+        {
+            put(OPTION_TYPE_MAPPER, W32APITypeMapper.UNICODE);
+            put(OPTION_FUNCTION_MAPPER, W32APIFunctionMapper.UNICODE);
+        }
+    });
+
+    boolean SystemParametersInfo(
+            WinDef.UINT_PTR uiAction,
+            WinDef.UINT_PTR uiParam,
+            String pvParam,
+            WinDef.UINT_PTR fWinIni
+    );
+}
